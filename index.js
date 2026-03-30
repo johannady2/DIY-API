ead70cf35c8e631e56cd0f67045ff70e752f3e63
johannady2 <johannady2@gmail.com>
2026-03-30 17:18:33 +0900

    delete all function completed

--- index.js
+++ index.js
@@ -187,6 +187,25 @@ app.delete("/jokes/:id", (req, res) =>
 
 
 //8. DELETE All jokes
+app.delete("/all", (req, res) => {
+
+  const userKey = req.query.key;
+
+  if(userKey === masterKey)
+  {
+    jokes.length = 0;
+    res.sendStatus(200);
+  }
+  else
+  {
+    res.status(404).json({error: "Unauthorized  request"});
+  }
+  
+});
+
+
+
+//
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
